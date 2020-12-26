de0730a21e4b2d1d7a948f643d83446877f2b437
Jan Sierakowski <jasiekxl@gmail.com>
2020-12-26 18:47:01 +0100

    Version 1

---
+++ CarTryApplication/ItemService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarTryApplication
+{
+    public class ItemService
+    {
+        public List<Item> Items { get; set; }
+
+        public ItemService()
+        {
+            Items = new List<Item>();
+        }
+
+        public ConsoleKeyInfo AddNewItemView(MenuActionService actionService, out string carBrand)
+        {
+            var addNewItemMenu = actionService.GetMenuActionsByName("AddNewItemMenu");
+            for (int i = 0; i < addNewItemMenu.Count; i++)
+            {
+                Console.WriteLine($"{addNewItemMenu[i].Id}. {addNewItemMenu[i].Name}");
+            }
+            var operation = Console.ReadKey();
+            int carBrandChoosed;
+            Int32.TryParse(operation.KeyChar.ToString(), out carBrandChoosed);
+            string carBrandChosed = addNewItemMenu[carBrandChoosed-1].Name;
+            carBrand = carBrandChosed;
+            return operation;
+        }
+
+        public int AddNewItem(char itemType, string carBrand)
+        {
+            int itemTypeId;
+            Int32.TryParse(itemType.ToString(), out itemTypeId);
+            Item item = new Item();
+            item.TypeId = itemTypeId;
+
+            item.CarBrand = carBrand;
+
+            Console.WriteLine("Enter id for new car announcement");
+            var id = Console.ReadLine();
+            int itemId;
+            Int32.TryParse(id, out itemId);
+
+            Console.WriteLine("Please enter your car model");
+            var model = Console.ReadLine();
+
+            item.Id = itemId;
+            item.CarModel = model;
+
+            Items.Add(item);
+            return itemId;
+        }
+
+        public int RemoveItemView()
+        {
+            Console.WriteLine("Enter id you want to delete: ");
+            var itemId = Console.ReadLine();
+            int id;
+            Int32.TryParse(itemId.ToString(), out id);
+            return id;
+        }
+
+        public void CarBrandsByTypeView(string carBrand)
+        {
+            List<Item> toShow = new List<Item>();
+            foreach (var item in Items)
+            {
+                if (item.CarBrand == carBrand)
+                {
+                    toShow.Add(item);
+                }
+            }
+            Console.WriteLine(toShow.ToStringTable(new[] { "Id", "Car Brand", "Car Model" }, a => a.Id, a => a.CarBrand, a => a.CarModel));
+        }
+
+        public string CarBrandTypeSelectionView()
+        {
+            Console.WriteLine("Enter car brand you want to show: ");
+            var carBrand = Console.ReadLine();
+            return carBrand;
+        }
+
+        public void RemoveItem(int removeId)
+        {
+            Item productToRemove = new Item();
+            foreach (var item in Items)
+            {
+                if (item.Id == removeId)
+                {
+                    productToRemove = item;
+                    break;
+                }
+            }
+
+            Items.Remove(productToRemove);
+
+        }
+
+        public void ItemDetailView(int detailId)
+        {
+            Item productToShow = new Item();
+            foreach (var item in Items)
+            {
+                if (item.Id == detailId)
+                {
+                    productToShow = item;
+                    break;
+                }
+            }
+            Console.WriteLine($"Car id: {productToShow.Id} ");
+            Console.WriteLine($"Car brand: {productToShow.CarBrand} ");
+            Console.WriteLine($"Car model: {productToShow.CarModel} ");
+        }
+
+        public int ItemDetailSelectionView()
+        {
+            Console.WriteLine("Enter id you want to show: ");
+            var itemId = Console.ReadLine();
+            int id;
+            Int32.TryParse(itemId.ToString(), out id);
+            return id;
+        }
+    }
+}
---
+++ CarTryApplication/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace CarTryApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //// c1.Wyszukanie ogłoszenia po filtrze marki/id
+            //// c2.Wybór ogłoszenia z listy
+            //// c3.Przedstawienie wybranego szczegółowego ogłoszenia 
+            ///
+
+            MenuActionService actionService = new MenuActionService();
+            actionService = Initialize(actionService);
+            ItemService itemService = new ItemService();
+            Console.WriteLine("Welocome in first car test&try community");
+            Console.WriteLine("Select what you want to do:");
+
+
+            while (true)
+            {
+                var mainMenu = actionService.GetMenuActionsByName("Main");
+                for (int i = 0; i < mainMenu.Count; i++)
+                {
+                    Console.WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
+                }
+                var operation = Console.ReadKey();
+                
+                Console.WriteLine();
+
+                switch (operation.KeyChar)
+                {
+                    case '1':
+                        string carBrand;
+                        var keyInfo = itemService.AddNewItemView(actionService,out carBrand);
+                        var id = itemService.AddNewItem(keyInfo.KeyChar, carBrand);
+                        break;
+                    case '2':
+                        var removeId = itemService.RemoveItemView();
+                        itemService.RemoveItem(removeId);
+                        break;
+                    case '3':
+                        var detailId = itemService.ItemDetailSelectionView();
+                        itemService.ItemDetailView(detailId);
+                        break;
+                    case '4':
+                        var brandToShow = itemService.CarBrandTypeSelectionView();
+                        itemService.CarBrandsByTypeView(brandToShow);
+                        break;
+                    default:
+                        Console.WriteLine("Action you entered does not exist");
+                        break;
+                }
+            }
+        }
+
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.AddNewAction(1, "Add the car that you can lend to test&try", "Main");
+            actionService.AddNewAction(2, "Delete a car test&try announcement", "Main");
+            actionService.AddNewAction(3, "Search a car to test&try", "Main");
+            actionService.AddNewAction(4, "Search a model by car brand to test&try", "Main");
+
+            actionService.AddNewAction(1, "Opel", "AddNewItemMenu");
+            actionService.AddNewAction(2, "Volkswagen", "AddNewItemMenu");
+            actionService.AddNewAction(3, "Toyota", "AddNewItemMenu");
+
+            return actionService;
+        }
+    }
+}
