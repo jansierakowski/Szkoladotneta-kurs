bdb07373b906d12685d96aa9e503abe8570ee95b
Jan Sierakowski <jasiekxl@gmail.com>
2021-01-06 19:13:14 +0100

    first version of application

---
+++ CarTryApplication/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarTryApplication
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string CarBrand { get; set; }
+        public string CarModel { get; set; }
+        public int TypeId { get; set; }
+
+    }
+}
--- CarTryApplication/Program.cs
+++ CarTryApplication/Program.cs
@@ -1,8 +1,4 @@
-﻿using CarTry.App.Abstract;
-using CarTry.App.Concrete;
-using CarTry.App.Manager;
-using CarTry.Domain.Entity;
-using System;
+﻿using System;
 
 namespace CarTryApplication
 {
@@ -10,11 +6,18 @@ namespace CarTryApplication
     {
         static void Main(string[] args)
         {
-            MenuActionService actionService = new MenuActionService();
-            ItemManger itemManger = new ItemManger(actionService);
+           
+            //// c1.Wyszukanie ogłoszenia po filtrze marki/id
+            //// c2.Wybór ogłoszenia z listy
+            //// c3.Przedstawienie wybranego szczegółowego ogłoszenia 
+            ///
 
-            Console.WriteLine("Welcome in first car test&try community");
+            MenuActionService actionService = new MenuActionService();
+            actionService = Initialize(actionService);
+            ItemService itemService = new ItemService();
+            Console.WriteLine("Welocome in first car test&try community");
             Console.WriteLine("Select what you want to do:");
+
 
             while (true)
             {
@@ -24,25 +27,31 @@ namespace CarTryApplication
                     Console.WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
                 }
                 var operation = Console.ReadKey();
-
+                
                 Console.WriteLine();
 
                 switch (operation.KeyChar)
                 {
                     case '1':
-                        var newId = itemManger.AddNewItem();
+                        string carBrand;
+                        var keyInfo = itemService.AddNewItemView(actionService,out carBrand);
+                        Console.WriteLine();
+                        var id = itemService.AddNewItem(keyInfo.KeyChar, carBrand);
                         break;
                     case '2':
-                        var removeId = itemManger.RemoveItem();
+                        var removeId = itemService.RemoveItemView();
                         Console.WriteLine();
+                        itemService.RemoveItem(removeId);
                         break;
                     case '3':
-                        var detailId = itemManger.ItemDetail();
+                        var detailId = itemService.ItemDetailSelectionView();
                         Console.WriteLine();
+                        itemService.ItemDetailView(detailId);
                         break;
                     case '4':
-                        var brandToShow = itemManger.CarBrandsByType();
+                        var brandToShow = itemService.CarBrandTypeSelectionView();
                         Console.WriteLine();
+                        itemService.CarBrandsByTypeView(brandToShow);
                         break;
                     default:
                         Console.WriteLine("Action you entered does not exist");
@@ -51,6 +60,18 @@ namespace CarTryApplication
             }
         }
 
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.AddNewAction(1, "Add the car that you can lend to test&try", "Main");
+            actionService.AddNewAction(2, "Delete a car test&try announcement", "Main");
+            actionService.AddNewAction(3, "Search a car to test&try", "Main");
+            actionService.AddNewAction(4, "Search a model by car brand to test&try", "Main");
 
+            actionService.AddNewAction(1, "Opel", "AddNewItemMenu");
+            actionService.AddNewAction(2, "Volkswagen", "AddNewItemMenu");
+            actionService.AddNewAction(3, "Toyota", "AddNewItemMenu");
+
+            return actionService;
+        }
     }
 }
